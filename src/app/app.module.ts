da9b3d40871b6d45e276ce38a1c21139c2efbc91
cavaquiro <cavaquiro@gmail.com>
2019-07-15 20:18:48 -0500

    firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { environment } from './../environments/environment'
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+     ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDgeBmx9iFpYZsk8kLAfoLQMOx2Yd5bzb0",
+    authDomain: "angular-final-app-88ba0.firebaseapp.com",
+    databaseURL: "https://angular-final-app-88ba0.firebaseio.com",
+    projectId: "angular-final-app-88ba0",
+    storageBucket: "angular-final-app-88ba0.appspot.com",
+    messagingSenderId: "896387923808",
+    appId: "1:896387923808:web:8e295b820fb6cb08"
+  }
+};
